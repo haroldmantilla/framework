1205dd205612806e29ceb941344c418b56db540e
ScottMayer <m183990@usna.edu>
2018-05-05 12:52:05 -0400

    added login.php

---
+++ login.php
@@ -0,0 +1,8 @@
+<?php
+
+  ///////////////////////////////////////////////////////////////
+  // Automatically redirect to the home page for this application
+
+  header("Location: home/home.php");
+
+?>
