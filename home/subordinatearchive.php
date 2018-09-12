7edd9bc6e39eac47b68e566f2d251b22e02e1673
Harold <m194020@usna.edu>
2018-09-12 16:38:33 -0400

    test

--- home/subordinatearchive.php
+++ home/subordinatearchive.php
@@ -170,10 +170,9 @@ elseif (!empty($subarchivedchits)){
           }
           elseif($mystatus == "DISAPPROVED"){
             echo "<td><button  style=\"cursor: auto !important\" type=\"button\" class=\"btn btn-danger\" disabled>DISAPPROVED</button></td>";
-          } else {
-            echo "<td class=\"min\"><button style=\"cursor: auto !important\" type=\"button\" class=\"btn btn-secondary\" disabled>Pending</button></td>";
           }
 
+          echo "<td class=\"min\"><button style=\"cursor: auto !important\" type=\"button\" class=\"btn btn-secondary\" disabled>Pending</button></td>";
 
 
 
