665b2f54a7a908212df76dc325b025664e45000f
Harold Mantilla <m194020@usna.edu>
2018-09-12 20:51:22 -0700

    test

--- home/test.php
+++ home/test.php
@@ -57,13 +57,13 @@
   $results = coc_complete($db, "m194020");
   echo "$results";
 
-  $to = "m193978@usna.edu";
-$subject = "A chit is ready for your approval!";
-$txt = "Log in at midn.cs.usna.edu/project-echits to review the chit.
-I am the better CS/IT major and platoon sam can eat my dust";
-$headers = "From: eChits@noreply.edu" . "\r\n" .
-"CC: m194020@usna.edu";
-
-sendmail($to,$subject,$txt,$headers);
+//   $to = "m193978@usna.edu";
+// $subject = "A chit is ready for your approval!";
+// $txt = "Log in at midn.cs.usna.edu/project-echits to review the chit.
+// I am the better CS/IT major and platoon sam can eat my dust";
+// $headers = "From: eChits@noreply.edu" . "\r\n" .
+// "CC: m194020@usna.edu";
+//
+// sendmail($to,$subject,$txt,$headers);
 
 ?>
