03ecf5ee2eb75a56b05f2104d4f306507067ed29
Harold <m194020@usna.edu>
2018-09-13 15:22:57 -0400

    test

--- home/help.php
+++ home/help.php
@@ -81,6 +81,25 @@ if(isset($_POST['sent']) && $_POST['sent']){
 <div class = "col-md-8">
 
 
+
+
+  <form action="mailto:m194020@example.com" method="post" enctype="text/plain">
+  Name:<br>
+  <input type="text" name="name"><br>
+  E-mail:<br>
+  <input type="text" name="mail"><br>
+  Comment:<br>
+  <input type="text" name="comment" size="50"><br><br>
+  <input type="submit" value="Send">
+  <input type="reset" value="Reset">
+  </form>
+
+
+
+
+
+
+
   <!-- Contact -->
   <section id="contact">
     <div class="container">
