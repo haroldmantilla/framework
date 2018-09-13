0b22df56ca3b679d955c2a0352ee4136b2559fa4
Harold Mantilla <m194020@usna.edu>
2018-09-12 23:22:57 -0700

    subordinate archive

--- home/subordinatearchive.php
+++ home/subordinatearchive.php
@@ -96,36 +96,49 @@ elseif (!empty($subarchivedchits)){
         echo "<td>{$chit['description']}</td>";
 
 
-
         $chitstatus = "PENDING";
-        if($chit['coc_0_status'] == "DISAPPROVED" ||
-           $chit['coc_1_status'] == "DISAPPROVED" ||
-           $chit['coc_2_status'] == "DISAPPROVED" ||
-           $chit['coc_3_status'] == "DISAPPROVED" ||
-           $chit['coc_4_status'] == "DISAPPROVED" ||
-           $chit['coc_5_status'] == "DISAPPROVED" ||
-           $chit['coc_6_status'] == "DISAPPROVED" ||
-           $chit['coc_7_status'] == "DISAPPROVED" ||
-           $chit['coc_8_status'] == "DISAPPROVED" ){
-          $chitstatus = "DISAPPROVED";
-        }
-        elseif($chitstatus != "DISAPPROVED"){
-          if(!empty($chit['coc_0_username'])){ //dant
-            $chitstatus = $chit['coc_0_status'];
-          }
-          elseif(!empty($chit['coc_1_username'])){ //depdant
-            $chitstatus = $chit['coc_1_status'];
-          }
-          elseif(!empty($chit['coc_2_username'])){ //batt-o
-            $chitstatus = $chit['coc_2_status'];
-          }
-          elseif(!empty($chit['coc_3_username'])){ // co
-            $chitstatus = $chit['coc_3_status'];
-          }
-          elseif(!empty($chit['coc_4_username'])){ //sel
-            $chitstatus = $chit['coc_4_status'];
-          }
-        }
+
+       if(!empty($chit['coc_2_username'])){ //batt-o
+         $chitstatus = $chit['coc_2_status'];
+
+       }
+       elseif(empty($chit['coc_2_username']) && !empty($chit['coc_3_username'])){ // co
+         $chitstatus = $chit['coc_3_status'];
+
+       }
+       elseif(empty($chit['coc_2_username']) && empty($chit['coc_3_username'])&&!empty($chit['coc_4_username'])){ //sel
+         $chitstatus = $chit['coc_4_status'];
+
+       }
+        // $chitstatus = "PENDING";
+        // if($chit['coc_0_status'] == "DISAPPROVED" ||
+        //    $chit['coc_1_status'] == "DISAPPROVED" ||
+        //    $chit['coc_2_status'] == "DISAPPROVED" ||
+        //    $chit['coc_3_status'] == "DISAPPROVED" ||
+        //    $chit['coc_4_status'] == "DISAPPROVED" ||
+        //    $chit['coc_5_status'] == "DISAPPROVED" ||
+        //    $chit['coc_6_status'] == "DISAPPROVED" ||
+        //    $chit['coc_7_status'] == "DISAPPROVED" ||
+        //    $chit['coc_8_status'] == "DISAPPROVED" ){
+        //   $chitstatus = "DISAPPROVED";
+        // }
+        // elseif($chitstatus != "DISAPPROVED"){
+        //   if(!empty($chit['coc_0_username'])){ //dant
+        //     $chitstatus = $chit['coc_0_status'];
+        //   }
+        //   elseif(!empty($chit['coc_1_username'])){ //depdant
+        //     $chitstatus = $chit['coc_1_status'];
+        //   }
+        //   elseif(!empty($chit['coc_2_username'])){ //batt-o
+        //     $chitstatus = $chit['coc_2_status'];
+        //   }
+        //   elseif(!empty($chit['coc_3_username'])){ // co
+        //     $chitstatus = $chit['coc_3_status'];
+        //   }
+        //   elseif(!empty($chit['coc_4_username'])){ //sel
+        //     $chitstatus = $chit['coc_4_status'];
+        //   }
+        // }
 
 
           if($chitstatus == "PENDING"){
