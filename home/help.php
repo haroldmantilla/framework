b1a7308c889ae26ec5475f0bd0ddb9a660dbaf13
Harold <m194020@usna.edu>
2018-09-13 16:04:01 -0400

    test

--- home/help.php
+++ home/help.php
@@ -141,8 +141,8 @@ isset($_POST['message'])){
               <div class="clearfix"></div>
               <div class="col-lg-12 text-center">
                 <div id="success"></div>
-                <button id="sendMessageButton" class="btn btn-xl" type="submit">Submit</button>
-                <button style="float: right;" type="button" class="btn btn-default" data-toggle="modal" data-target="#whoModal">Who should I route my chit to?</button>
+                <button id="sendMessageButton" class="btn btn-xl" type="submit">Submit<button type="button" class="btn btn-default" data-toggle="modal" data-target="#whoModal"></button></button>
+
 
               </div>
             </div>
