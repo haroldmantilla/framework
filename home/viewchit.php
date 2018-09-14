a135b234b1c0699adb7fa5dcdc3ba30f1ef64220
Harold <m194020@usna.edu>
2018-09-14 15:18:30 -0400

    plz

--- home/viewchit.php
+++ home/viewchit.php
@@ -102,8 +102,21 @@
       //  "CC: m194020@usna.edu"; // THIS IS FOR FUTURE USE
 
       sendemail($to,$subject,$txt,$headers); // ACTUALLY SENDS EMAIL
+    } else {                                    // not the last person in the CoC
+      $aggregate = $aboveCoC."_username";  // grab whatever coc it is
 
+      $coc_email = get_user_information($db, $chit['creator']);            // grab coc email
+      //$to = "m194020@usna.edu";
+      $to = "{$chit[''.$aggregate.'']}@usna.edu";                                     // who to send email to
+      //{$chit['creator']} this is who it should send to eventually
+      $subject = "A chit is ready for your review.";                               // SUBJECT OF THE EMAIL
+
+      $txt = "Chit by {$coc_email['firstName']} {$coc_email['lastName']}, {$coc_email['service']}
+      \nLog in at midn.cs.usna.edu/project-echits to review the chit. \n";
+
+      $headers = "From: eChits@noreply.usna" . "\r\n";                             // IT WILL SEND FROM THIS ADDRESS
     }
+
     $today = date("dMy");
     $today = strtoupper($today);
     $now = date("Hi");
@@ -291,7 +304,7 @@
      //{$chit['creator']} this is who it should send to eventually
     $subject = "A comment has been left on your chit.";                               // SUBJECT OF THE EMAIL
 
-    $txt = "{$coc_email['rank']} {$coc_email['firstName']} {$coc_email['lastName']}, {$coc_email['service']} said:
+    $txt = "{$coc_email['rank']} {$coc_email['firstName']} {$coc_email['lastName']}, {$coc_email['service']} commented:
           $comments.
           \nLog in at midn.cs.usna.edu/project-echits to review the chit. \n";
 
