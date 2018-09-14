6e46624cebbabed43db23fcd705c65de2a74b390
Harold <m194020@usna.edu>
2018-09-14 13:44:29 -0400

    ttt

--- home/viewchit.php
+++ home/viewchit.php
@@ -234,11 +234,11 @@
 
     $aggregate = $who."_username";  // grab whatever coc it is
     $coc_email = get_user_information($db, $chit[''.$aggregate.'']);            // grab coc email
-    //$to = "m194020@usna.edu";
-    $before = "{$chit['creator']}@usna.edu";
-    $to = $before;                                        // who to send email to
+    $to = "m194020@usna.edu";
+    //$to = "{$chit['creator']}@usna.edu";
+                                            // who to send email to
      //{$chit['creator']} this is who it should send to eventually
-    $subject = "this was a test for comments.";                               // SUBJECT OF THE EMAIL
+    $subject = "Content is $to.";                               // SUBJECT OF THE EMAIL
 
     $txt = "Disapproved by {$coc_email['rank']} {$coc_email['firstName']} {$coc_email['lastName']}, {$coc_email['service']}
           \nLog in at midn.cs.usna.edu/project-echits to review the chit. \n";
