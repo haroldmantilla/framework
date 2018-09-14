d0502eb788ff4471782305ee1af32c40f5c26028
Harold <m194020@usna.edu>
2018-09-14 14:58:38 -0400

    ?

--- home/viewchit.php
+++ home/viewchit.php
@@ -87,27 +87,27 @@
 
     action($db, $chit, $who, "APPROVED", $today, $now);
 
-    // $aggregate = $aboveCoC."_username";
-    //
-    // if(!isset($chit[''.$aggregate.''])){ // if this is the last person in CoC:
-    //
-    //   $aggregate = $who."_username";  // grab whatever coc it is
-    //
-    //   $coc_email = get_user_information($db, $chit[''.$aggregate.'']);            // grab coc email
-    //   //$to = "m194020@usna.edu";
-    //   $to = "{$chit['creator']}@usna.edu";                                        // who to send email to
-    //   //{$chit['creator']} this is who it should send to eventually
-    //   $subject = "Your chit has been approved!";                               // SUBJECT OF THE EMAIL
-    //
-    //   $txt = "Final approval by {$coc_email['rank']} {$coc_email['firstName']} {$coc_email['lastName']}, {$coc_email['service']}
-    //   \nLog in at midn.cs.usna.edu/project-echits to review the chit. \n";
-    //
-    //   $headers = "From: eChits@noreply.usna" . "\r\n";                             // IT WILL SEND FROM THIS ADDRESS
-    //
-    //   //  $headers = "From: eChits@noreply.edu" . "\r\n" . // IT WILL SEND FROM THIS ADDRESS
-    //   //  "CC: m194020@usna.edu"; // THIS IS FOR FUTURE USE
-    //
-    //   sendemail($to,$subject,$txt,$headers); // ACTUALLY SENDS EMAIL
+    $aggregate = $aboveCoC."_username";
+
+    if(!isset($chit[''.$aggregate.''])){ // if this is the last person in CoC:
+
+      $aggregate = $who."_username";  // grab whatever coc it is
+
+      $coc_email = get_user_information($db, $chit[''.$aggregate.'']);            // grab coc email
+      //$to = "m194020@usna.edu";
+      $to = "{$chit['creator']}@usna.edu";                                        // who to send email to
+      //{$chit['creator']} this is who it should send to eventually
+      $subject = "Your chit has been approved!";                               // SUBJECT OF THE EMAIL
+
+      $txt = "Final approval by {$coc_email['rank']} {$coc_email['firstName']} {$coc_email['lastName']}, {$coc_email['service']}
+      \nLog in at midn.cs.usna.edu/project-echits to review the chit. \n";
+
+      $headers = "From: eChits@noreply.usna" . "\r\n";                             // IT WILL SEND FROM THIS ADDRESS
+
+      //  $headers = "From: eChits@noreply.edu" . "\r\n" . // IT WILL SEND FROM THIS ADDRESS
+      //  "CC: m194020@usna.edu"; // THIS IS FOR FUTURE USE
+
+      sendemail($to,$subject,$txt,$headers); // ACTUALLY SENDS EMAIL
 
     }
     //redirect
